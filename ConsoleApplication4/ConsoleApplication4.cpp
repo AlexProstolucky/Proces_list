2d4b767205b7d9e31d168fa21b27b9b7aebe6225
AlexProstolucky <alexprostolucky@gmail.com>
2023-10-19 20:55:13 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <tlhelp32.h>
+
+void ListProcesses() {
+    HANDLE hProcessSnap;
+    PROCESSENTRY32 pe32;
+
+    hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hProcessSnap == INVALID_HANDLE_VALUE) {
+        std::cerr << "Помилка при створенні знімку процесів" << std::endl;
+        return;
+    }
+
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+
+    if (!Process32First(hProcessSnap, &pe32)) {
+        std::cerr << "Помилка при отриманні інформації про процес" << std::endl;
+        CloseHandle(hProcessSnap);
+        return;
+    }
+
+    do {
+        std::cout << "PID: " << pe32.th32ProcessID << " Назва: " << pe32.szExeFile << std::endl;
+    } while (Process32Next(hProcessSnap, &pe32));
+
+    CloseHandle(hProcessSnap);
+}
+
+int main() {
+    SetConsoleOutputCP(1251);
+    int interval = 5000; /
+
+    while (true) {
+        ListProcesses();
+        Sleep(interval); 
+
+    return 0;
+}
